1791c67b555c63dfcd7f1a1f7650abfae0b4fd71
Paul C <paulcollinsaf@gmail.com>
2019-10-06 18:45:10 +1000

    week9: minor bugfix

--- server/routes/product.js
+++ server/routes/product.js
@@ -3,25 +3,18 @@ var router = express.Router();
 var bodyParser = require("body-parser");
 var ProductModel = require("../model");
 
-router.get("/",bodyParser.json(), function(req, res, next) {
-  if (!req.query) {
-    return res.sendStatus(400);
-  }
-
-  ProductModel.findById(req.query._id, (err, body) => {
-    res.send(body);
+router.get("/",bodyParser.urlencoded({extended: false}),   function(req, res, next) {
+  ProductModel.findById(req.query._id, (err, doc) => {
+    res.send(doc);
   });
 });
 
-router.get("/all",bodyParser.json(), function(req, res, next) {
-    if (!req.body) {
-      return res.sendStatus(400);
-    }
-  
-    ProductModel.find((err, body) => {
-      res.send(body);
-    });
+router.get("/all", function(req, res, next) {
+  ProductModel.find((err, docs) => {
+    res.send(docs);
   });
+});
+
 
 router.post("/", bodyParser.json(), function(req, res, next) {
   // Create an instance of model SomeModel
@@ -43,23 +36,17 @@ router.delete("/:_id", function(req, res, next) {
 });
 
 
-router.put("/", bodyParser.json(), function(req, res, next) {
-  if (!req.body) {
-    console.log('req.body is null');
-    return res.sendStatus(400)
-  }
-
+router.post("/update", bodyParser.json(), function(req, res, next) {
     ProductModel.findById(req.body._id,(err,doc) => {
       if (!doc) {
         console.log('findById doc is null');
         return res.sendStatus(400)
       }
-
+        doc.Id = req.body.Id;
         doc.Name = req.body.Name;
         doc.Description = req.body.Description;
         doc.Price = req.body.Price;
         doc.Units = req.body.Units;
-
         doc.save(function (err) {
             if (err)  {
               console.log('save failed');
